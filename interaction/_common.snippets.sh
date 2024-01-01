46dfa0737a48dedaa78b9b09b0e392d3267fe6ef
Aurélien Passion <aurelien.passion@gmail.com>
2024-01-01 18:07:35 +0100

    update interaction scripts

--- interaction/_common.snippets.sh
+++ interaction/_common.snippets.sh
@@ -5,6 +5,72 @@
 echo "Proxy: ${PROXY}"
 echo "SC address: ${SC_ADDRESS:-Not deployed}"
 
+SC_ADDRESS_HEX="$(mxpy wallet bech32 --decode "${SC_ADDRESS}")"
+NFT_COLLECTION_ID_HEX="$(echo -n "${NFT_COLLECTION_ID}" | xxd -ps)"
+
+##
+# Post deployment
+##
+
+setNftCreateRole() {
+    ROLE="$(echo -n "ESDTRoleNFTCreate" | xxd -ps)"
+    SET_ROLE_DATA="setSpecialRole@${NFT_COLLECTION_ID_HEX}@${SC_ADDRESS_HEX}@${ROLE}"
+    SET_ROLE_ADDRESS=erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u
+
+    echo 'Collection owner must send the following tx:'
+    echo "receiver: ${SET_ROLE_ADDRESS}"
+    echo 'value: 0 EGLD'
+    echo 'gas limit: 51000000'
+    echo 'data:'
+    echo "${SET_ROLE_DATA}"
+}
+
+transferNftCreateRole() {
+    read -p "From address: " FROM_ADDRESS
+    FROM_ADDRESS_HEX="$(mxpy wallet bech32 --decode "${FROM_ADDRESS}")"
+
+    ROLE="$(echo -n "ESDTRoleNFTCreate" | xxd -ps)"
+    SET_ROLE_DATA="transferNFTCreateRole@${NFT_COLLECTION_ID_HEX}@${FROM_ADDRESS_HEX}@${SC_ADDRESS_HEX}"
+    SET_ROLE_ADDRESS=erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u
+
+    echo 'Collection owner must send the following tx:'
+    echo "receiver: ${SET_ROLE_ADDRESS}"
+    echo 'value: 0 EGLD'
+    echo 'gas limit: 51000000'
+    echo 'data:'
+    echo "${SET_ROLE_DATA}"
+}
+
+setAdmin() {
+    read -p "Admin address: " ADMIN_ADDRESS
+
+    mxpy contract call ${SC_ADDRESS} \
+        --function="setAdmin" \
+        --arguments "${ADMIN_ADDRESS}" \
+        --gas-limit=10000000 \
+        --keyfile=${1} --proxy=${PROXY} --chain=${CHAIN} --recall-nonce --send || return
+}
+
+##
+# Admin
+##
+
+createProposal() {
+    read -p "ID (JIP number): " PROP_ID
+    read -p "Label: " LABEL
+    read -p "Start vote timestamp (s): " START_VOTE_TIMESTAMP
+    read -p "End vote timestamp (s): " END_VOTE_TIMESTAMP
+    read -p "Nb choice: " NB_CHOICES
+    read -p "Proposal content tx hash: " CONTENT_TX_HASH
+
+    mxpy contract call ${SC_ADDRESS} \
+        --function="createProposal" \
+        --arguments "${PROP_ID}" "str:${LABEL}" \
+            "${START_VOTE_TIMESTAMP}" "${END_VOTE_TIMESTAMP}" \
+            "${NB_CHOICES}" "0x${CONTENT_TX_HASH}" \
+        --gas-limit=10000000 \
+        --pem=${1} --proxy=${PROXY} --chain=${CHAIN} --recall-nonce --send || return
+}
 
 ##
 # Views
@@ -17,6 +83,21 @@ isVoter() {
         --function "isVoter" \
         --arguments "${ADDRESS}" \
         --proxy=${PROXY}
+}
+
+getAdminAddress() {
+    mxpy contract query ${SC_ADDRESS} --function "getAdminAddress" --proxy=${PROXY} \
+        | jq -r .[].hex
+}
+
+getProposals() {
+    FIRST=${1:-1}
+    LAST=${2:-10}
+
+    mxpy contract query ${SC_ADDRESS} \
+        --function "getProposals" --arguments "${FIRST}" "${LAST}" \
+        --proxy=${PROXY} \
+        | jq -r .[].hex
 }
 
 getVotingPower() {
--- interaction/devnet.snippets.sh
+++ interaction/devnet.snippets.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-BYTECODE=../output/jex-sc-governance.wasm
+BYTECODE=../output-docker/jex-sc-governance/jex-sc-governance.wasm
 PROXY=https://devnet-gateway.multiversx.com
 SC_ADDRESS=$(mxpy data load --key=address-devnet)
 CHAIN=D
 SCRIPT_DIR=$(dirname $0)
 SC_LOCKER_ADDRESS=erd1qqqqqqqqqqqqqpgqmmxzmktd09gq0hldtczerlv444ykt3pz6avsnys6m9
+NFT_COLLECTION_ID=JIP-c2a017
 
 source "${SCRIPT_DIR}/_common.snippets.sh"
 
+# Reproducible build using:
+# mxpy contract reproducible-build --docker-image="multiversx/sdk-rust-contract-builder:v5.3.0"
 deploy() {
     echo 'You are about to deploy SC on devnet (Ctrl-C to abort)'
     read answer
 
     mxpy contract deploy --bytecode=${BYTECODE} \
         --keyfile=${1} --gas-limit=50000000 --outfile="deploy-devnet.interaction.json" \
-        --arguments "${SC_LOCKER_ADDRESS}" \
+        --arguments "${SC_LOCKER_ADDRESS}" "str:${NFT_COLLECTION_ID}" \
         --proxy=${PROXY} --chain=${CHAIN} --recall-nonce --send || return
-
-    SC_ADDRESS=$(mxpy data parse --file="deploy-devnet.interaction.json" --expression="data['contractAddress']")
+    
+    SC_ADDRESS=$(cat deploy-devnet.interaction.json | jq -r .contractAddress)
 
     mxpy data store --key=address-devnet --value=${SC_ADDRESS}
 
@@ -32,7 +35,6 @@ upgrade() {
 
     mxpy contract upgrade --bytecode=${BYTECODE} \
         --keyfile=${1} --gas-limit=50000000 --outfile="deploy-devnet.interaction.json" \
-        --arguments "${SC_LOCKER_ADDRESS}" \
         --proxy=${PROXY} --chain=${CHAIN} --recall-nonce --send ${SC_ADDRESS} || return
 
     echo ""
